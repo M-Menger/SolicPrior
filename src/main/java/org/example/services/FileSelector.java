d9d3805cfe11c032cfec0a9a155c1a1421583400
Matheus Menger <matheus-menger@hotmail.com>
2025-05-28 20:54:33 -0300

    Ajuste realizado para iniciar na pasta downlaods

--- src/main/java/org/example/services/FileSelector.java
+++ src/main/java/org/example/services/FileSelector.java
@@ -8,6 +8,15 @@ public class FileSelector {
     public static File selectFile() {
         JFileChooser fileChooser = new JFileChooser();
 
+        String userHome = System.getProperty("user.home");
+        File downloadsDir = new File(userHome, "Downloads");
+
+        if (downloadsDir.exists() && downloadsDir.isDirectory()){
+            fileChooser.setCurrentDirectory(downloadsDir);
+        } else {
+            fileChooser.setCurrentDirectory(new File(userHome));
+        }
+
         int returnValue = fileChooser.showOpenDialog(null);
 
         if (returnValue == JFileChooser.APPROVE_OPTION) {
